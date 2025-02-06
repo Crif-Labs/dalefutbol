6ffedac71b29b5f942904ab0aea179582ea32e80
Felipe Jara <fjara9613@gmail.com>
2025-02-05 22:05:13 -0300

    First Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "dale-futbol", appId: "1:259286345543:web:1e7f1fe628480f6fe14e62", databaseURL: "https://dale-futbol-default-rtdb.firebaseio.com", storageBucket: "dale-futbol.firebasestorage.app", apiKey: "AIzaSyAKPbzFospdPnLWEHmlw4lQg6ZKWghdwEE", authDomain: "dale-futbol.firebaseapp.com", messagingSenderId: "259286345543", measurementId: "G-XVDWBT7J3L" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideAnimationsAsync()]
 };
