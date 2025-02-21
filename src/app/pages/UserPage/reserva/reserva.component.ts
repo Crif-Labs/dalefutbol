33df853e14cc2ac8bb5573c2c96cee5618d4c67a
Crif-Labs <crif.labs@gmail.com>
2025-02-20 22:51:53 -0300

    2 day reserva

--- src/app/pages/UserPage/reserva/reserva.component.ts
+++ src/app/pages/UserPage/reserva/reserva.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Storage, ref, listAll, getDownloadURL  } from '@angular/fire/storage';
+import { Cancha } from '../../../interfaces/cancha';
+import { response } from 'express';
 
 @Component({
   selector: 'app-reserva',
@@ -11,10 +13,109 @@ import { Storage, ref, listAll, getDownloadURL  } from '@angular/fire/storage';
 export class ReservaComponent {
 
   images: string[];
+  profile_photo: string[];
+
+  teams = [
+    {
+      name: 'Equipo 1',
+      color: 'white',
+      players: [
+        {
+          name: 'Carlo Magno',
+          url_photo_profile: ''
+        },
+        {
+          name: 'Atila el Huno',
+          url_photo_profile: ''
+        },
+        {
+          name: 'Ragnar Lothbrok',
+          url_photo_profile: ''
+        },
+        {
+          name: 'William Wallace',
+          url_photo_profile: ''
+        }
+      ]
+    },
+    {
+      name: 'Equipo 2',
+      color: 'blue',
+      players: [
+        {
+          name: 'Carlo V',
+          url_photo_profile: 'gs://dale-futbol.firebasestorage.app/profile-photos/Profile-2.webp'
+        },
+        {
+          name: 'Napoleon Bonaparte',
+          url_photo_profile: 'gs://dale-futbol.firebasestorage.app/profile-photos/Profile-2.webp'
+        },
+        {
+          name: 'Juana de Arco',
+          url_photo_profile: 'gs://dale-futbol.firebasestorage.app/profile-photos/Profile-2.webp'
+        }
+      ]
+    }
+
+  ]
+
+  dataCancha: Cancha = {
+    nombre: 'SuperCancha',
+    direccion: 'San Ricardo, La Pintana',
+    capacidad: 7,
+    precio: 4700
+  }
+
+  dataReserva = [
+    {
+      icon: 'fa-solid fa-stopwatch',
+      detail: '21:00 hr'
+    },
+    {
+      icon: 'fa-solid fa-calendar-days',
+      detail: '11/02'
+    },
+    {
+      icon: 'fa-regular fa-hourglass-half',
+      detail: '60 min'
+    },
+    {
+      icon: 'fa-solid fa-dollar-sign',
+      detail: this.dataCancha.precio
+    }
+  ]
+
+  formatMatch = [
+    {
+      category: 'medio',
+      text: "Juego de 2 equipos de 7, 60 minutos en total."
+    },
+    {
+      category: 'medio',
+      text: "Juego para todo nivel y todo genero."
+    },
+    {
+      category: 'important',
+      text: "Solo jugadores inscritos pueden jugar este partido."
+    },
+    {
+      category: 'medio',
+      text: "El minimo de jugadores es de 10 en total (5 por equipo), si no el partido sera cancelado."
+    },
+    {
+      category: 'medio',
+      text: "Por favor llegar con 10 minutos de anticipacion para la preparacion del partido; esto podria afectar a la duracion de tu juego."
+    }
+    
+    
+    
+  ]
 
   constructor(private storage: Storage){
     this.getPhoto()
+    this.getProfilePhoto()
     this.images = []
+    this.profile_photo = []
   }
 
   getPhoto(){
@@ -30,13 +131,36 @@ export class ReservaComponent {
             
         }
 
-        console.log( this.images)
-
       }).catch( (error) => {
         console.log(error)
       })
+  }
 
-    
+  getProfilePhoto(){
+    const photoProfileRef = ref(this.storage, '/profile-photos')
+
+    listAll(photoProfileRef)
+      .then( async (response) => {
+        this.profile_photo = []
+
+        for(let item of response.items){
+          const url = await getDownloadURL(item)
+          this.profile_photo.push(url)
+        }
+
+
+        let i = 0
+        for(let team of this.teams){
+
+          for(let player of team.players){
+            player.url_photo_profile = this.profile_photo[i]
+          }
+
+          i++
+        }
+      }).catch( (error) => {
+        console.log(error)
+      })
   }
 
 }
